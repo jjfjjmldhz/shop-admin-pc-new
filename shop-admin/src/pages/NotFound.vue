1a1ad9db350f2dff2ccb8c975faf881579d1aa1f
ywz101 <ywz101@gmail.com>
2023-08-14 21:19:26 +0800

    404页面美化成功

--- shop-admin/src/pages/NotFound.vue
+++ shop-admin/src/pages/NotFound.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { useRoute } from 'vue-router'
+// router: 内部有一些方法, 如router.push()
+// route: 内部有一些参数, 如route.params.pathMatch
+
+const route = useRoute()
+const errorTip = `您输入的路径 "${route.params.pathMatch}" 不存在~`
 
 </script>
 
 <template>
   <div class="not-found">
-    <h2>not-found组件</h2>
+    <el-result
+        icon="warning"
+        title="404页面"
+        :sub-title="errorTip"
+      >
+        <template #extra>
+          <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+        </template>
+      </el-result>
   </div>
 </template>
 
